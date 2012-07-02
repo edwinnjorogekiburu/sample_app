6dbbd231f656d928476883aeb1d7a261ebec9a5a
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-07-02 17:35:01 +0300

    Add User following

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    
+    private 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_many :microposts , dependent: :destroy ,:order => "id DESC"
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   has_secure_password
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -19,13 +23,24 @@ class User < ActiveRecord::Base
       self.remember_token = SecureRandom.urlsafe_base64
     end
 
-public
-  def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    microposts
-  end
+    public
 
+    def feed
+      # This is preliminary. See "Following users" for the full implementation.
+      Micropost.from_users_followed_by(self)
+    end
 
+    def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+    end
+
+    def follow!(other_user)
+      relationships.create!(followed_id: other_user.id)
+    end
+    
+    def unfollow!(other_user)
+      relationships.find_by_followed_id(other_user.id).destroy
+    end
 
 
 
